1830a019e9a719adc536f8c4a646952fe015828c
Denis <denisoreshkoofficial@gmail.com>
2023-10-26 12:59:51 +0300

    2.0.0-beta.2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Node js Railway!');
+  res.end('Node js Railway beta 2!');
 });
 
 server.listen(PORT, () => {
