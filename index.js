c7f8a6ca1aa87842d0992ae20e0b246c3004810a
Denis <denisoreshkoofficial@gmail.com>
2023-10-26 13:02:37 +0300

    1.6.1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const PORT = 3000;
-//const brain = require('brain.js')
+const brain = require('brain.js')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Node js Railway beta 2!');
+  res.end('Node js Railway 1.6.1!');
 });
 
 server.listen(PORT, () => {
