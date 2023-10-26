00ff91faf5b1645627233b671efbe3497af4b1b6
Denis <denisoreshkoofficial@gmail.com>
2023-10-26 16:30:22 +0300

    1.6.1 net b

--- index.js
+++ index.js
@@ -27,9 +27,35 @@ function scaleUp(step, min, max){
       five: step.five*(max-min)+min
   };
 }
+// Create a MySQL database connection
+const dbConfig = {
+  host: 'viaduct.proxy.rlwy.net:27786',
+  user: 'root',
+  password: 'a6-CF-dB-cEGDeDbH4GahhB-FaB62eFd',
+  database: 'railway',
+};
+
+const conn = mysql.createConnection(dbConfig);
+
+conn.connect((err) => {
+  if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+  }
+  console.log('Connected to the database');
+});
 
 app.use(bodyParser.json());
 app.get('/', (req, res) => {  
+  const sql = 'SELECT * FROM `536`';
+  conn.query(sql, (err, results) => {
+    if (err) {
+        console.error('Error executing query: ' + err);
+        res.status(500).json({ error: 'Database query failed' });
+        return;
+    }
+    res.json(results);        
+  });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.send('Node js Railway 1.6.1 brain.js!');
