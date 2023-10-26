cb2d4882e71d8ab295d29386167445fc2e451c7c
Denis <denisoreshkoofficial@gmail.com>
2023-10-26 13:04:16 +0300

    1.6.1

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const brain = require('brain.js')
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  const net = new brain.recurrent.LSTMTimeStep({
+    inputSize: 5,
+    hiddenLayers: [15,25,25],
+    outputSize: 5
+  });
   res.end('Node js Railway 1.6.1!');
 });
 
